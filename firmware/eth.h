0eadc031405bbeae0142e4fd0575f9f731b55cb1
rpedde <ron.pedde@rackspace.com>
2011-03-21 12:54:52 -0500

    Fix eth header packing

--- firmware/eth.h
+++ firmware/eth.h
@@ -5,14 +5,16 @@
 #define ETH_HDR_LEN     14
 #define ETH_HEADER_LEN  14
 
-typedef uint8_t* eth_addr_t;
+typedef struct eth_addr_t {
+    uint8_t data[ETH_ADDR_LEN];
+} eth_addr_t;
 
 /* don't really care beyond these */
 #define ETH_TYPE_IP     0x0800
 #define ETH_TYPE_ARP    0x0806
 #define ETH_TYPE_RARP   0x8035
 
-#define ETH_ADDR_BCAST  (uint8_t*)"\xff\xff\xff\xff\xff\xff"
+#define ETH_ADDR_BCAST  (eth_addr_t*)"\xff\xff\xff\xff\xff\xff"
 
 typedef struct eth_header_t {
     eth_addr_t dst_addr;
@@ -20,6 +22,6 @@ typedef struct eth_header_t {
     uint16_t eth_type;
 } eth_header_t;
 
-extern void eth_pack_hdr(eth_header_t *packet, eth_addr_t dst, eth_addr_t src, uint16_t type);
+extern void eth_pack_hdr(eth_header_t *packet, eth_addr_t *dst, eth_addr_t *src, uint16_t type);
 
 #endif /* _ETH_H_ */
