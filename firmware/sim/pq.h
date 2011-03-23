ff3e44f1b8f9128390581f052839409704e679b6
rpedde <ron@pedde.com>
2011-03-22 23:43:13 -0500

    DHCP renewals working, state machine still wonky

--- firmware/sim/pq.h
+++ firmware/sim/pq.h
@@ -10,6 +10,6 @@ extern int pq_packet_available(void);
 extern int pq_fetch(char **packet, int *len);
 extern void pq_exit(void);
 extern void pq_inject(char *packet, int len);
-extern void pq_packet_wait(void);
+extern int pq_packet_wait(void);
 
 #endif /* _PQ_H_ */
